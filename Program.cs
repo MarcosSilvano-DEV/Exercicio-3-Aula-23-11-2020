dc8d8aad272211552a41103666f7936982da6fe8
Marcos Silvano <marcossilvano@gmail.com>
2020-12-01 16:13:58 -0300

    Exercicio 3 Aula 23-11-2020

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicio_3_Aula_23_11_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o preço: ");
+            float preco = float.Parse(Console.ReadLine());
+
+            TotalAPagar(quantidade,preco);
+
+            float TotalAPagar(int quantidade, float preco)
+            {
+                float total = quantidade * preco;
+                float desconto;
+                if(quantidade <= 5){
+                    desconto = 0.02f;
+                }else if(quantidade <= 10){
+                    desconto = 0.03f;
+                }else{
+                    desconto = 0.05f;
+                }
+                float valorDoDesconto = total * desconto;
+                float totalComDesconto = total - valorDoDesconto;
+
+                Console.WriteLine($"O valor total é de: {total}");
+                Console.WriteLine($"Você receberá: R${valorDoDesconto} de desconto");
+                Console.WriteLine($"Sua compra com desconto fica: R${totalComDesconto}");
+                return totalComDesconto;
+            }
+        }
+    }
+}
